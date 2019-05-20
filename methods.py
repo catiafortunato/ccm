f9525bf1c5b82a8088c26c46ab486010f29f2608
catiafortunato <catiafortunato35@gmail.com>
2019-05-20 14:00:10 +0100

    self prediction function

--- methods.py
+++ methods.py
@@ -10,9 +10,27 @@ def build_shadow_M (X,tau,E,T):
     for i in range((tau*E-1),T):
         sample=np.zeros((E))
         for j in range(0,E):
-            sample[j]=x[i-j*tau]
+            sample[j]=X[i-j*tau]
         shadow_M[i-(tau*E-1),:]=sample
     return shadow_M
+
+def build_shadow_selfpred (X,tau,E,T,tpred):
+    '''Build the shadow manifold of the time series signal X, with E variables and sampling tau and the shadow manifold of the time series signal X+tpred to perform self prediction'''
+    shadow_X=np.zeros((T-E+1-tpred,E))
+    for i in range((tau*E-1),T-tpred):
+        sample=np.zeros((E))
+        for j in range(0,E):
+            sample[j]=X[i-j*tau]
+        shadow_X[i-(tau*E-1),:]=sample
+    
+    shadow_Xpred=np.zeros((T-E+1-tpred,E))
+    for i in range((tau*E+tpred-1),T):
+        sample=np.zeros((E))
+        for j in range(0,E):
+            sample[j]=X[i-j*tau]
+        shadow_Xpred[i-(tau*E-1)-tpred,:]=sample
+    
+    return shadow_X,shadow_Xpred
 
 def sample_manifold (M, L):
     '''Randomly select L points from the shadow manifold M'''
@@ -38,7 +56,6 @@ def nearest_points(shadow_x,M,idx,E):
             distances_t[i,:]=distances[k[0],1:E+3]
             indices_t[i,:]=indices[k[0],1:E+3]
         else:
-            np.concatenate((a,[[1,2,4]]),axis=0)
             new_M=np.concatenate((M,[shadow_x[i,:]]))
             nbrs=NearestNeighbors(n_neighbors=E+2,algorithm='kd_tree',metric='euclidean').fit(new_M)
             distances, indices=nbrs.kneighbors(new_M)
@@ -51,7 +68,7 @@ def nearest_points(shadow_x,M,idx,E):
             indices_t[i,j]=idx[indices_t[i,j]]
     return distances_t, indices_t
 
-def compute_weights(distances,indices,T,eps=1e-4):
+def compute_weights(distances,indices,T,E,eps=1e-4):
     weights=np.zeros((T,E+1))
     weights_u=np.zeros((T,E+1))
     for i in range (len(distances)):
@@ -95,7 +112,7 @@ def compute_xmap(X,Y,T,E,tau,L):
     distances_x, indices_x=nearest_points(shadow_x,recon_Mx,idx_x,E)
     
     # compute weights
-    weights_w_x=compute_weights(distances_x,indices_x,T)
+    weights_w_x=compute_weights(distances_x,indices_x,T,E)
     
     # compute prediction
     My_pred,My_target,y_pred, y_target=compute_prediction(shadow_y,weights_w_x,E,tau,T,indices_x)
@@ -106,7 +123,7 @@ def compute_xmap(X,Y,T,E,tau,L):
     distances_y, indices_y=nearest_points(shadow_y, recon_My,idx_y,E)
     
     # compute weights
-    weights_w_y=compute_weights(distances_y,indices_y,T)
+    weights_w_y=compute_weights(distances_y,indices_y,T,E)
     
     # compute prediction 
     Mx_pred,Mx_target,x_pred, x_target=compute_prediction(shadow_x,weights_w_y,E,tau,T,indices_y)
